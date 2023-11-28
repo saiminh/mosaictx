c9fdebb440ff3d692bd511678f82ce671eaed209
saiminh <saiminh@gmail.com>
2023-11-28 18:18:32 +0100

    viewport meta tag

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ export const Layout = (props) => {
       <Head>
         <title>Tina App</title>
         <meta name="description" content="A TinaCMS Application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>{props.children}</main>
