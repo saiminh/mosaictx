04032e3f5d2ecc5d2a763ed74f7158340268c9bc
saiminh <saiminh@gmail.com>
2023-12-04 16:19:21 +0100

    add tina fields

--- components/blocks/PartnershipsInvestors.js
+++ components/blocks/PartnershipsInvestors.js
@@ -10,12 +10,12 @@ export const PartnershipsInvestors = (props) => {
     <div className={styles.partnershipsInvestors}>
       <div className={styles.text}>
         <h2 className={styles.title} data-tina-field={tinaField(props.data, 'title')}>{props.data.title}</h2>
-        <div className={styles.body}>
+        <div className={styles.body} data-tina-field={tinaField(props.data, 'body')}>
           <TinaMarkdown content={props.data.body} />
         </div>
       </div>
       <div className={styles.logos}>
-        <div className={styles.investor}>
+        <div className={styles.investor} data-tina-field={tinaField(props.data, 'linkToSynconia')}>
           <a href={props.data.linkToSynconia || ''}>
             <figure className={`${styles.logo} ${styles.logo__synconia__image}`}>
               <SynconaLogo />
@@ -28,7 +28,7 @@ export const PartnershipsInvestors = (props) => {
             backgroundColor='green'
           />
         </div>
-        <div className={styles.investor}>
+        <div className={styles.investor} data-tina-field={tinaField(props.data, 'linkToCIT')}>
           <a href={props.data.linkToCIT || ''}>
             <figure className={`${styles.logo} ${styles.logo__cic__image}`}>
               <CICLogo />
