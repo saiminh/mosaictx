a04f012051300c03ee0d647163edee316cdd0576
saiminh <saiminh@gmail.com>
2023-12-07 19:13:58 +0100

    add meta stuff

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,22 @@
-import Link from 'next/link'
 import Head from 'next/head'
 
 export const Layout = (props) => {
+
+  const meta = {
+    title: props.title || 'MosaicTX - The Combination Oncology People™',
+    description: props.description || 'Resolving cancer’s complexity, through the power of combination therapeutics.',
+    metaimg: props.metaimg || '/favicon.ico'
+  }
+
   return (
     <div>
       <Head>
-        <title>Tina App</title>
-        <meta name="description" content="A TinaCMS Application" />
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.metaimg} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>{props.children}</main>
