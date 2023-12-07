043738c58b12b3e23f0200b5fa143ed6148b2655
saiminh <saiminh@gmail.com>
2023-12-07 19:15:19 +0100

    if content

--- components/blocks/PartnershipsInvestors.js
+++ components/blocks/PartnershipsInvestors.js
@@ -9,33 +9,33 @@ export const PartnershipsInvestors = (props) => {
   return (
     <div className={styles.partnershipsInvestors}>
       <div className={styles.text}>
-        <h2 className={styles.title} data-tina-field={tinaField(props.data, 'title')}>{props.data.title}</h2>
+        <h2 className={styles.title} data-tina-field={tinaField(props.data, 'title')}>{props.data?.title}</h2>
         <div className={styles.body} data-tina-field={tinaField(props.data, 'body')}>
-          <TinaMarkdown content={props.data.body} />
+          <TinaMarkdown content={props.data?.body} />
         </div>
       </div>
       <div className={styles.logos}>
         <div className={styles.investor} data-tina-field={tinaField(props.data, 'linkToSynconia')}>
-          <a href={props.data.linkToSynconia || ''}>
+          <a href={props.data?.linkToSynconia || ''}>
             <figure className={`${styles.logo} ${styles.logo__synconia__image}`}>
               <SynconaLogo />
             </figure>
           </a>
           <Button 
-            href={props.data.linkToSynconia || ''} 
+            href={props.data?.linkToSynconia || ''} 
             className={styles.logo__synconia__link} 
             text='visit' 
             backgroundColor='green'
           />
         </div>
         <div className={styles.investor} data-tina-field={tinaField(props.data, 'linkToCIT')}>
-          <a href={props.data.linkToCIT || ''}>
+          <a href={props.data?.linkToCIT || ''}>
             <figure className={`${styles.logo} ${styles.logo__cic__image}`}>
               <CICLogo />
             </figure>
           </a>
           <Button 
-            href={props.data.linkToCIT || ''} 
+            href={props.data?.linkToCIT || ''} 
             className={styles.logo__cit__link} 
             text='visit' 
             backgroundColor='green'
