630879be022ae8e5068c34edb3f05cf171d9aa43
saiminh <saiminh@gmail.com>
2023-12-19 22:50:07 +0100

    forced redirect from netlify

--- next.config.js
+++ next.config.js
@@ -15,20 +15,4 @@ module.exports = {
       },
     ]
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: 'https://mosaictx-website.netlify.app/',
-        permanent: true,
-        basePath: false,
-      },
-      {
-        source: '/admin',
-        destination: 'https://mosaictx-website.netlify.app/admin',
-        permanent: true,
-        basePath: false,
-      },
-    ]
-  },
 }
